41b6f7dff2f8754bde8072cce56d2cd8e8ec0f30
AtrishHub <gunjanatrish32@gmail.com>
2025-05-28 19:08:11 +0530

    new voice-chat2 page

--- src/app/text-to-speech/page.tsx
+++ src/app/text-to-speech/page.tsx
@@ -5,7 +5,7 @@ import TTSComponent from "@/components/TTSComponent";
 export default function tts() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-gray-100 p-4">
-      <TTSComponent initialText="Hello, this is a test of browser text to speech in Next.js!" />
+      <TTSComponent/>
     </main>
   );
 }
